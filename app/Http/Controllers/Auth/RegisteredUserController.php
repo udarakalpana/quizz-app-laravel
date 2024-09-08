f7cadf2f4f3e45eb130a0cabefd234a6ab06941a
kalpana <kalpanaudara058@gmail.com>
2024-09-08 12:10:13 +0530

    Create seeder for roles and assign user roles when register new user.

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -32,6 +32,7 @@ class RegisteredUserController extends Controller
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+            'user_role' => ['required', 'in:admin,user'],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
@@ -41,10 +42,13 @@ class RegisteredUserController extends Controller
             'password' => Hash::make($request->password),
         ]);
 
-        event(new Registered($user));
+        if ($request->user_role === 'admin') {
+            $user->assignRole('admin');
+        }
+        else {
+            $user->assignRole('user');
+        }
 
-        Auth::login($user);
-
-        return redirect(route('dashboard', absolute: false));
+        return redirect(route('login', absolute: false));
     }
 }
