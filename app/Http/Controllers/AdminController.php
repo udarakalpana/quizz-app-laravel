78bf3cfc47b990d9629479dc4bcf01d001dfe2de
kalpana <kalpanaudara058@gmail.com>
2024-09-29 10:47:24 +0530

    Fetch questions with answers and show them all in the blade view

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Answer;
+use App\Models\Question;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Foundation\Application;
@@ -11,6 +13,14 @@ class AdminController extends Controller
 {
     public function showDashboard(): View|Factory|Application
     {
-        return view('admin.dashboard');
+
+//        https://laravel-news.com/laravel-model-tips
+        // lazy loading attempt
+//        $questions = Question::all();
+
+        // Eager loading attempt
+        $questions = Question::with('answers')->get();
+
+        return view('admin.dashboard')->with(['questions' => $questions]);
     }
 }
